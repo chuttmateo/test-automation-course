cab29d727e1f2a95165014c3f16dbe9d8a26577f
chuttmateo <chuttmateo0@gmail.com>
2023-10-23 17:13:17 -0300

    Main.class has been added

--- first-lecture/src/Main.java
+++ first-lecture/src/Main.java
@@ -5,23 +5,23 @@ public class Main {
         //1. Create a simple system.out app.
         // Print info about the user that should be passed to the main function as a parameter.
         // Compile and run the program from the command line using javac and java from JDK.
-
+        System.out.println("Parameters: ");
         for (String arg: args) {
             System.out.println(arg);
         }
 
         //2. Sorting algorithm implementation.
         // Choose any from the list: Insertion sort, Selection sort, bubble sort, Merge sort, Quicksort.
-
+        System.out.println("Unsorted array: ");
         int[] nums = {1,4,2,3,5,9,8,6,7};
         for (int n : nums){
             System.out.println(n);
         }
 
-       System.out.println("Bubble sort");
+       System.out.println("Array sorted by bubble sort algorithm: ");
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length - 1; j++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
                 if ( nums[j] > nums[j + 1]){
                     int aux = nums[j];
                     nums[j] = nums[j+1];
@@ -30,6 +30,9 @@ public class Main {
             }
         }
 
-        Arrays.stream(nums).forEach(System.out::println);
+        for (int n : nums){
+            System.out.println(n);
+        }
+
     }
 }
