66919b54b0f7cb4e6448682c150ee2fd6f90be3b
chuttmateo <chuttmateo0@gmail.com>
2023-11-13 10:18:42 -0300

    Custom linked list added

--- third-lecture/src/laba/homework/SixthHomework.java
+++ third-lecture/src/laba/homework/SixthHomework.java
@@ -1,10 +1,30 @@
 package laba.homework;
 
+import laba.homework.list.CustomLinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class SixthHomework {
     /*Add 5 collections to the hierarchy.
     Create custom LinkedList with generic.*/
     public static void main(String[] args) {
 
+        CustomLinkedList<Integer> custom = new CustomLinkedList<>();
+
+        System.out.println("Is empty: " + custom.isEmpty());
+        System.out.println("Size: " + custom.size());
+
+        custom.add(1);
+        custom.add(3);
+        custom.add(2);
+        custom.add(6);
+
+        System.out.println("Contains 1: " + custom.contains(1));
+
+        System.out.println("Size: " + custom.size());
+
+        System.out.println("Is empty: " + custom.isEmpty());
 
 
     }
---
+++ third-lecture/src/laba/homework/list/CustomLinkedList.java
@@ -0,0 +1,146 @@
+package laba.homework.list;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class CustomLinkedList<T> implements List<T> {
+    Node<T> head;
+    int size;
+
+    @Override
+    public boolean add(T t) {
+        Node<T> newNode = new Node<>(t);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        size++;
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (head == null) return false;
+
+        while (head.next != null) {
+            if (o.equals(head.data)) return true;
+            head = head.next;
+        }
+
+        return o.equals(head.data);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
